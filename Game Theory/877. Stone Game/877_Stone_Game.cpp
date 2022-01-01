f18a29b59108c9b7b90b4f42a842ec1b5414ce19
Pin Loon Lee <pinloon_0428@hotmail.com>
2022-01-01 17:11:18 +0800

    Game Theory: added 877 Stone Game

---
+++ Game Theory/877. Stone Game/877_Stone_Game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        // sort(piles.begin(), piles.end(), greater<int>());
+        // int a {};
+        // int b {};
+        // for (int i = 0; i < piles.size(); ++i) {
+        //     if (i % 2 == 0) {
+        //         a += piles[i];
+        //     } else {
+        //         b += piles[i];
+        //     }
+        // }
+        // return a > b;
+        return true;
+    }
+};
